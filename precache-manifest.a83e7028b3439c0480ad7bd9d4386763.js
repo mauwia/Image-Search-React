e211c727b9f13a43c8c78e06dafd179f1d7f5fd1
Mavia <mauwia.atif@gmail,com>
2019-09-23 13:42:03 +0500

    Updates

---
+++ precache-manifest.a83e7028b3439c0480ad7bd9d4386763.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3861b36b255c3e157b0c6c1279c3d8e1",
+    "url": "/Image-Search-React/index.html"
+  },
+  {
+    "revision": "940e989703555e43b339",
+    "url": "/Image-Search-React/static/css/main.4621d794.chunk.css"
+  },
+  {
+    "revision": "dc4644de5faf670bd0fa",
+    "url": "/Image-Search-React/static/js/2.bf1553fb.chunk.js"
+  },
+  {
+    "revision": "940e989703555e43b339",
+    "url": "/Image-Search-React/static/js/main.6af8561c.chunk.js"
+  },
+  {
+    "revision": "61d597f200b81fe0d35e",
+    "url": "/Image-Search-React/static/js/runtime-main.5e2a09bf.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Image-Search-React/precache-manifest.a83e7028b3439c0480ad7bd9d4386763.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Image-Search-React/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
